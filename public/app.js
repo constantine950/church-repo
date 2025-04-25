a1976d6124a8885a3d8b4442d3b4e73c079f7495
james <christymack950@gmail.com>
2025-04-25 15:46:25 +0100

    v2

--- public/app.js
+++ public/app.js
@@ -1,24 +1,24 @@
-class Dropdown{
-    constructor(container){
-        this.container = container;
-        this.trigger = container.querySelector('.trigger');
-        this.content = container.querySelector('.content');
-    }
-    init(){
-        this.trigger.addEventListener('click', () => {
-            this.content.classList.toggle('hidden')
-        });
-    };
+class Dropdown {
+  constructor(container) {
+    this.container = container;
+    this.trigger = container.querySelector(".trigger");
+    this.content = container.querySelector(".content");
+  }
+  init() {
+    this.trigger.addEventListener("click", () => {
+      this.content.classList.toggle("hidden");
+    });
+  }
 }
 
-const dropdowns = document.querySelectorAll('.dropdown');
-dropdowns.forEach(dropdown => {
-    const instance = new Dropdown(dropdown);
-    instance.init();
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
 });
 
-const trigger_1 = document.querySelector('.trigger-1');
-const content_1 = document.querySelector('.content-1');
-trigger_1.addEventListener('click', () => {
-    content_1.classList.toggle('hidden');
+const trigger_1 = document.querySelector(".trigger-1");
+const content_1 = document.querySelector(".content-1");
+trigger_1.addEventListener("click", () => {
+  content_1.classList.toggle("hidden");
 });
